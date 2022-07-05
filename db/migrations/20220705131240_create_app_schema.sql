0c69e62bce67680e8be1bfbac19992c90578e2b9
Alexey Yuchenko <alexes.dev@gmail.com>
2022-07-05 16:32:10 +0300

    initial commit

---
+++ db/migrations/20220705131240_create_app_schema.sql
@@ -0,0 +1,7 @@
+-- migrate:up
+
+create schema app;
+
+-- migrate:down
+
+drop schema app;
---
+++ db/migrations/20220705131358_create_users_table.sql.sql
@@ -0,0 +1,21 @@
+-- migrate:up
+
+create type app.gender as enum (
+  'male',
+  'female'
+);
+
+create table app.users (
+  id serial primary key,
+  email text not null,
+  name text,
+  phone text,
+  sex app.gender,
+
+  constraint proper_email check (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+);
+
+-- migrate:down
+
+drop table app.users;
+drop type app.gender;
